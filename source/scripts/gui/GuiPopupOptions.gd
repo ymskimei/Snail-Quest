b2ad07d996161dc1c93decf787f3be4358c7079c
Mei <ymskimei@gmail.com>
2023-09-15 20:33:58 -0700

    Text moved to translations, randomization test

--- source/scripts/gui/GuiPopupOptions.gd
+++ source/scripts/gui/GuiPopupOptions.gd
@@ -1,13 +1,11 @@
 extends Popup
 
-onready var fullscreen = $Tabs/Video/Margin/Grid/CheckBoxFullscreen
-onready var filter = $Tabs/Video/Margin/Grid/ButtonFilter
-onready var resolution = $Tabs/Video/Margin/Grid/ButtonResolution
-onready var framerate = $Tabs/Video/Margin/Grid/ButtonFramerate
-onready var vsync = $Tabs/Video/Margin/Grid/CheckBoxVsync
-onready var master_volume = $Tabs/Audio/Margin/Grid/BarMasterVolume
-onready var music_volume = $Tabs/Audio/Margin/Grid/BarMusicVolume
-onready var sfx_volume = $Tabs/Audio/Margin/Grid/BarSfxVolume
+onready var fullscreen: CheckBox = $Tabs/Video/Margin/Grid/CheckBoxFullscreen
+onready var filter: OptionButton = $Tabs/Video/Margin/Grid/ButtonFilter
+onready var resolution: OptionButton = $Tabs/Video/Margin/Grid/ButtonResolution
+onready var framerate: OptionButton = $Tabs/Video/Margin/Grid/ButtonFps
+onready var vsync: CheckBox = $Tabs/Video/Margin/Grid/CheckBoxVsync
+onready var language: OptionButton = $Tabs/Misc/Margin/Grid/ButtonLanguage
 
 var resolution_dict: Dictionary = {
 	"2560x1440 (16:9)": Vector2(2560, 1440),
@@ -33,82 +31,91 @@ var framerate_dict: Dictionary = {
 	"120 FPS": 120
 }
 
-var last_focus
-var new_focus
-var mono_audio : bool
+var language_dict: Dictionary = {
+	"English (United States)": "en_US",
+	"English (Great Britain)": "en_GB",
+	"Español": "es_MX",
+	"Rioplatense": "es_AR",
+	"日本語": "ja_JP",
+	"Pirate": "pr"
+}
 
-func _ready():
+var last_focus: Control
+var new_focus: Control
+var mono_audio: bool
+
+func _ready() -> void:
 	add_resolution()
 #	if GlobalManager.screen != null:
 #		add_filter()
 	add_framerate()
+	add_language()
 	#get_default_focus()
 
-func _unhandled_input(event):
+func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("gui_left"):
 		$Tabs.current_tab -= 1
 	if Input.is_action_just_pressed("gui_right"):
 		$Tabs.current_tab += 1
 
-func get_default_focus():
+func get_default_focus() -> void:
 	for tab in $Tabs.get_children():
 		for controls in tab.get_node("Margin").get_node("Grid").get_children():
 			if controls is BaseButton or Range:
 				controls.connect("focus_entered", self, "_on_button_focus_entered")
 
-func get_buttons(tab):
+func get_buttons(tab: Control):
 	for n in tab.get_node("Margin").get_node("Grid").get_children():
 		if n is BaseButton or Range:
 			print("found")
 			return n
 
-func _on_Tabs_tab_changed(tab):
+func _on_Tabs_tab_changed() -> void:
 #	if last_focus:
 #		last_focus.grab_focus()
-	for controls in $Tabs.get_child(tab).get_node("Margin").get_node("Grid").get_children():
+	for controls in $Tabs.get_children():
 		if controls is BaseButton or Range:
-			print("real")
 			controls.set_focus_mode(2)
 			controls.grab_focus()
 			break
 
-func _on_button_focus_entered(control):
+func _on_button_focus_entered() -> void:
 	last_focus = $Tabs.get_focus_owner()
 
 ## Video Settings
-func _on_CheckBoxFullscreen_toggled(button_pressed):
+func _on_CheckBoxFullscreen_toggled(button_pressed: bool) -> void:
 	OS.window_fullscreen = !OS.window_fullscreen
 
-func _on_ButtonResolution_item_selected(index):
+func _on_ButtonResolution_item_selected(index: int) -> void:
 	select_resolution(index)
 
-func _on_ButtonFilter_item_selected(index):
+func _on_ButtonFilter_item_selected(index: int) -> void:
 	select_filter(index)
 
-func _on_ButtonFramerate_item_selected(index):
+func _on_ButtonFps_item_selected(index: int) -> void:
 	var fps = framerate_dict.get(framerate.get_item_text(index))
 	Engine.set_target_fps(fps)
 
-func _on_CheckBoxVsync_toggled(button_pressed):
+func _on_CheckBoxVsync_toggled(button_pressed: bool) -> void:
 	OS.vsync_enabled = !OS.vsync_enabled
 
 ## Audio Settings
-func _on_BarMasterVolume_value_changed(value):
+func _on_BarVolumeMaster_value_changed(value: float) -> void:
 	var index = AudioServer.get_bus_index("Master")
 	AudioServer.set_bus_volume_db(index, linear2db(value))
 
-func _on_BarMusicVolume_value_changed(value):
+func _on_BarVolumeMusic_value_changed(value: float) -> void:
 	var index = AudioServer.get_bus_index("Music")
 	AudioServer.set_bus_volume_db(index, linear2db(value))
 
-func _on_BarSfxVolume_value_changed(value):
+func _on_BarVolumeSfx_value_changed(value: float) -> void:
 	var index = AudioServer.get_bus_index("SFX")
 	AudioServer.set_bus_volume_db(index, linear2db(value))
 
-func _on_CheckBoxHeadphones_toggled(button_pressed):
+func _on_CheckBoxHeadphones_toggled(button_pressed: bool) -> void:
 	pass
 
-func _on_CheckBoxMono_toggled(button_pressed):
+func _on_CheckBoxMono_toggled(button_pressed: bool) -> void:
 	var mode = AudioServer.get_bus_effect(AudioServer.get_bus_index("Mode"), 0)
 	var mast = AudioServer.get_bus_effect(AudioServer.get_bus_index("Master"), 0)
 	if !mono_audio:
@@ -120,7 +127,11 @@ func _on_CheckBoxMono_toggled(button_pressed):
 		mast.pan = 0
 		mono_audio = false
 
-func add_resolution():
+## Misc Settings
+func _on_ButtonLanguage_item_selected(index: int) -> void:
+	select_language(index)
+
+func add_resolution() -> void:
 	var current_resolution = get_viewport().get_size()
 	var index = 0
 	for r in resolution_dict:
@@ -129,7 +140,7 @@ func add_resolution():
 			resolution._select_int(index)
 		index += 1
 
-func add_filter():
+func add_filter() -> void:
 	var current_filter = GlobalManager.screen.get_type()
 	var index = 0
 	for r in filter_dict:
@@ -138,7 +149,7 @@ func add_filter():
 			filter._select_int(index)
 		index += 1
 
-func add_framerate():
+func add_framerate() -> void:
 	var current_fps = Engine.get_frames_per_second()
 	var index = 0
 	for r in framerate_dict:
@@ -147,14 +158,28 @@ func add_framerate():
 			framerate._select_int(index)
 		index += 1
 
-func select_resolution(index):
+func add_language() -> void:
+	var current_lang = TranslationServer.get_locale()
+	var index = 0
+	for r in language_dict:
+		language.add_item(r, index)
+		if language_dict[r] == current_lang:
+			language._select_int(index)
+		index += 1
+
+func select_resolution(index: int) -> void:
 	var size = resolution_dict.get(resolution.get_item_text(index))
 	OS.set_window_size(size)
 	get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_VIEWPORT, SceneTree.STRETCH_ASPECT_KEEP, size)
 
-func select_filter(index):
-	var size = filter_dict.get(filter.get_item_text(index))
-	GlobalManager.screen.set_type(index)
+func select_filter(index: int) -> void:
+	var filter = filter_dict.get(filter.get_item_text(index))
+	GlobalManager.screen.set_type(filter)
+
+func select_language(index: int) -> void:
+	var lang = language_dict.get(language.get_item_text(index))
+	TranslationServer.set_locale(lang)
+	print("Language set to: " + lang)
 
 #func _on_ButtonRight_pressed():
 #	$Tabs.current_tab += 1
@@ -163,6 +188,3 @@ func select_filter(index):
 #func _on_ButtonLeft_pressed():
 #	$Tabs.current_tab -= 1
 #	#current = clamp(current, 0, 3)
-
-func _on_TextureButtonAction_button_down():
-	pass # Replace with function body.
