46826065626860205a2fc2967d603b98a3528076
Mei <ymskimei@gmail.com>
2023-11-28 12:47:28 -0700

    Menu focus behaviors

--- source/scripts/GlobalManager.gd
+++ source/scripts/GlobalManager.gd
@@ -2,14 +2,22 @@ extends Node
 
 var version_number = "0.3.0-pre-alpha"
 
+var game_focused: bool = true
+
 var screen : Node
 var player : KinematicBody
 var camera : SpringArm
 var aim_cursor : Spatial
 var play_time : Node
 var game_time : Node
+
 var chunk_start : Vector3
 var chunk_size = 64
+
+var control_invert_vertical: bool = false
+var control_invert_horizontal: bool = false
+
+signal game_focus(is_focused)
 
 func register_screen(node):
 	screen = node
@@ -31,3 +39,12 @@ func register_game_time(node):
 
 func register_chunk_start(vec):
 	chunk_start = vec
+
+func _notification(what):
+	match what:
+		NOTIFICATION_WM_FOCUS_IN:
+			game_focused = true
+			emit_signal("game_focus", game_focused)
+		NOTIFICATION_WM_FOCUS_OUT:
+			game_focused = false
+			emit_signal("game_focus", game_focused)
--- source/scripts/gui/GuiCursor.gd
+++ source/scripts/gui/GuiCursor.gd
@@ -1,8 +1,12 @@
-extends TextureRect
+extends CanvasLayer
+
+onready var cursor: TextureRect = $Cursor
+onready var anim: AnimationPlayer = $Cursor/AnimationPlayer
 
 func _ready():
 	get_viewport().connect("gui_focus_changed", self, "_on_gui_focus_changed")
+	anim.play("CursorPulse")
 
 func _on_gui_focus_changed(node: Node):
-	rect_position = node.rect_position
+	cursor.rect_position = node.rect_global_position + (node.rect_size * 0.75)
 	print(str(node))
