60d7051c85385dcc3e254e7dde96bbe30a67c7b2
Mei <ymskimei@gmail.com>
2024-02-02 18:04:31 -0700

    More options added. Stickers and hats also included

--- snailgen/SnailGen.gd
+++ snailgen/SnailGen.gd
@@ -2,19 +2,17 @@ extends RigidBody
 
 export var identity: Resource = null
 
-onready var armature = $Armature
-onready var collision = $CollisionShape
-onready var shell = $Armature/Skeleton/MeshInstance
-onready var body = $Armature/Skeleton/Body
-onready var eye_left = $Armature/Skeleton/BoneAttachment2/EyeLeft
-onready var eye_right = $Armature/Skeleton/BoneAttachment/EyeRight
+onready var armature: Spatial = $Armature
+onready var collision: CollisionShape = $CollisionShape
+onready var shell: MeshInstance = $Armature/Skeleton/MeshInstance
+onready var body: MeshInstance = $Armature/Skeleton/Body
+onready var eye_left: MeshInstance = $Armature/Skeleton/BoneAttachment2/EyeLeft
+onready var eye_right: MeshInstance = $Armature/Skeleton/BoneAttachment/EyeRight
+onready var hat: MeshInstance = $Armature/Skeleton/BoneAttachment3/Hat
+onready var sticker: MeshInstance = $Armature/Skeleton/BoneAttachment3/Sticker
+onready var anim: AnimationPlayer = $AnimationPlayer
 
 var rng: RandomNumberGenerator = RandomNumberGenerator.new()
-
-var entity_name: String = "Snail"
-
-var body_size: float = 1
-var shell_size: float = 1
 
 var shell_mat: Material = null
 var shell_accent_mat: Material = null
@@ -25,6 +23,8 @@ var eye_left_mat: Material = null
 var eye_right_mat: Material = null
 var eyelid_left_mat: Material = null
 var eyelid_right_mat: Material = null
+var hat_mat: Material = null
+var sticker_mat: Material = null
 
 var eyes_left: Array = []
 var eyes_right: Array = []
@@ -34,21 +34,14 @@ var bodies: Array = []
 var body_patterns: Array = []
 var shells: Array = []
 var shell_patterns: Array = []
+var stickers: Array = []
+var hats: Array = []
+var hat_patterns: Array = []
 
 var lightener = Color("FFF4DB")
 var darkener = Color("0E0B3A")
 
 func _ready() -> void:
-	var meshes: String = "res://assets/model/"
-	var textures: String = "res://assets/texture/entity/"
-	eyes_left.append_array(Utility.get_loaded_files(meshes, "snail_eye_left", ".mesh"))
-	eyes_right.append_array(Utility.get_loaded_files(meshes, "snail_eye_right", ".mesh"))
-	eye_patterns.append_array(Utility.get_loaded_files(textures, "snail_eyes", ".png"))
-	eyelid_patterns.append_array(Utility.get_loaded_files(textures, "snail_eyelids", ".png"))
-	bodies.append_array(Utility.get_loaded_files(meshes, "snail_body", ".mesh"))
-	body_patterns.append_array(Utility.get_loaded_files(textures, "snail_body_accent", ".png"))
-	shells.append_array(Utility.get_loaded_files(meshes, "snail_shell", ".mesh"))
-	shell_patterns.append_array(Utility.get_loaded_files(textures, "snail_shell_accent", ".png"))
 	shell_mat = shell.get_surface_material(0)
 	shell_accent_mat = shell.get_surface_material(0).get_next_pass()
 	shell_body_mat = shell.get_surface_material(1)
@@ -58,11 +51,15 @@ func _ready() -> void:
 	eye_right_mat = eye_right.get_surface_material(0).get_next_pass()
 	eyelid_left_mat = eye_left.get_surface_material(0).get_next_pass().get_next_pass()
 	eyelid_right_mat = eye_right.get_surface_material(0).get_next_pass().get_next_pass()
+	hat_mat = hat.get_surface_material(0)
+	sticker_mat = sticker.get_surface_material(0)
+	_update_appearance_arrays()
 	lightener.a = 0.4
 	darkener.a = 0.2
+	anim.play("SnailIdle")
 
 func _physics_process(delta: float) -> void:
-	update_appearance()
+	_update_appearance()
 
 func set_entity_name(new_text: String) -> void:
 	identity.entity_name = new_text
@@ -70,27 +67,20 @@ func set_entity_name(new_text: String) -> void:
 func set_entity_scale(value: float) -> void:
 	identity.entity_scale = value / 50
 
-func set_entity_eyes(next: bool = true):
-	identity.mesh_eye_left = eyes_left[Utility.array_cycle(eye_left.get_mesh(), eyes_left, next)]
-	identity.mesh_eye_right = eyes_right[Utility.array_cycle(eye_right.get_mesh(), eyes_right, next)]
-
-func set_entity_pupils(next: bool = true):
-	identity.pattern_eyes = eye_patterns[Utility.array_cycle(eye_right_mat.get_shader_param("texture_albedo"), eye_patterns, next)]
-
-func set_entity_eyelids(next: bool = true):
-	identity.pattern_eyelids = eyelid_patterns[ Utility.array_cycle(eyelid_right_mat.get_shader_param("texture_albedo"), eyelid_patterns, next)]
+func set_entity_shell(next: bool = true):
+	identity.mesh_shell = shells[Utility.array_cycle(shell.get_mesh(), shells, next)]
+	_reposition_outfit()
 
 func set_entity_body(next: bool = true):
 	identity.mesh_body = bodies[Utility.array_cycle(body.get_mesh(), bodies, next)]
 
-func set_entity_pattern(next: bool = true):
-	identity.pattern_body = body_patterns[Utility.array_cycle(body_accent_mat.get_shader_param("texture_albedo"), body_patterns, next)]
-
-func set_entity_shell(next: bool = true):
-	identity.mesh_shell = shells[Utility.array_cycle(shell.get_mesh(), shells, next)]
+func set_entity_eyes(next: bool = true):
+	identity.mesh_eye_left = eyes_left[Utility.array_cycle(eye_left.get_mesh(), eyes_left, next)]
+	identity.mesh_eye_right = eyes_right[Utility.array_cycle(eye_right.get_mesh(), eyes_right, next)]
 	
-func set_entity_swirl(next: bool = true):
-	identity.pattern_shell = shell_patterns[Utility.array_cycle(shell_accent_mat.get_shader_param("texture_albedo"), shell_patterns, next)]
+func set_entity_hat(next: bool = true):
+	identity.mesh_hat = hats[Utility.array_cycle(hat.get_mesh(), hats, next)]
+	identity.pattern_hat = hat_patterns[Utility.array_cycle(hat_mat.get_shader_param("texture_albedo"), hat_patterns, next)]
 
 func set_color_shell(c: Color) -> void:
 	identity.color_shell_base = c
@@ -107,16 +97,38 @@ func set_color_body_accent(c: Color) -> void:
 func set_color_eyes(c: Color) -> void:
 	identity.color_eyes = c
 
+func set_entity_pupils(next: bool = true):
+	identity.pattern_eyes = eye_patterns[Utility.array_cycle(eye_right_mat.get_shader_param("texture_albedo"), eye_patterns, next)]
+
+func set_entity_eyelids(next: bool = true):
+	identity.pattern_eyelids = eyelid_patterns[ Utility.array_cycle(eyelid_right_mat.get_shader_param("texture_albedo"), eyelid_patterns, next)]
+
+func set_entity_pattern(next: bool = true):
+	identity.pattern_body = body_patterns[Utility.array_cycle(body_accent_mat.get_shader_param("texture_albedo"), body_patterns, next)]
+
+func set_entity_swirl(next: bool = true):
+	identity.pattern_shell = shell_patterns[Utility.array_cycle(shell_accent_mat.get_shader_param("texture_albedo"), shell_patterns, next)]
+
+func set_entity_sticker(next: bool = true):
+	identity.pattern_sticker = stickers[Utility.array_cycle(sticker_mat.get_shader_param("texture_albedo"), stickers, next)]
+
 func run_randomizer() -> void:
 	randomize()
 	set_entity_name(_get_random_name())
 	set_entity_scale(get_ramped_random_amount(20, 100))
-	for i in _get_random_amount():
-		set_entity_eyes()
+	if randi() % 5 == 1:
+		for i in _get_random_amount():
+			set_entity_eyes()
+	else:
+		identity.mesh_eye_left = eyes_left[0]
+		identity.mesh_eye_right = eyes_right[0]
 	for i in _get_random_amount():
 		set_entity_pupils()
-	for i in _get_random_amount():
-		set_entity_eyelids()
+	if randi() % 5 == 1:
+		for i in _get_random_amount():
+			set_entity_eyelids()
+	else:
+		identity.pattern_eyelids = eyelid_patterns[0]
 	for i in _get_random_amount():
 		set_entity_body()
 	for i in _get_random_amount():
@@ -125,6 +137,17 @@ func run_randomizer() -> void:
 		set_entity_shell()
 	for i in _get_random_amount():
 		set_entity_swirl()
+	if randi() % 6 == 1:
+		for i in _get_random_amount():
+			set_entity_sticker()
+	else:
+		identity.pattern_sticker = stickers[0]
+	if randi() % 10 == 1:
+		for i in _get_random_amount():
+			set_entity_hat()
+	else:
+		identity.mesh_hat = hats[0]
+		identity.pattern_hat = hat_patterns[0]
 	var body_color = _get_random_color()
 	var shell_color = _get_random_color()
 	var shell_color_accent = _get_random_color()
@@ -140,7 +163,7 @@ func run_randomizer() -> void:
 		shell_color_accent = shell_color_accent.blend(darkener)
 	if body_color.is_equal_approx(shell_color):
 		body_color = body_color.inverted()
-	if randi() % 2:
+	if !randi() % 3 == 1:
 		shell_color_accent = shell_color.blend(darkener).darkened(0.2)
 	set_color_body(body_color)
 	set_color_body_accent(_get_random_color().blend(Color(body_color.r, body_color.g, body_color.b, 0.4)))
@@ -149,7 +172,7 @@ func run_randomizer() -> void:
 	set_color_eyes(_get_random_color())
 
 func save_resource(path) -> void:
-	var full_path = path + identity.entity_name.to_lower() + ".res"
+	var full_path = path + identity.entity_name.to_lower() + ".tres"
 	ResourceSaver.save(full_path, identity)
 
 func load_resource(path) -> void:
@@ -157,7 +180,7 @@ func load_resource(path) -> void:
 
 func _get_random_amount() -> int:
 	randomize()
-	var num = int(rand_range(1, int(rand_range(10, 20))))
+	var num = rng.randi_range(1, rng.randi_range(10, 20))
 	return num
 
 func get_ramped_random_amount(minimum: float, maximum: float):
@@ -180,15 +203,15 @@ func _get_random_name() -> String:
 		"blobby", "dubbington", "klonk", "shwinkle", "slurpo",
 		"amonculous", "weezer", "haru", "jumbo", "quaggle",
 		"bozo", "gay", "bloob", "ginort", "nooty",
-		"snooty", "king", "mimi", "kwartz", "kai", "phep",
+		"snooty", "king", "mimi", "kwartz", "kai", "pheph",
 		"chaos", "armageddon", "delta", "shelby", "bob"]
 	var blacklisted_names: Array = [
-		"chud", "jap", "klan", "slur"]
+		"chud", "jap", "klan", "slur", "vag", "smut", ""]
 	var fallback_name: String = possible_names[randi() % possible_names.size()].capitalize()
 	if randi() % 100 <= 95:
 		var created_name = _make_random_name()
 		if created_name.length() >= 3 and !blacklisted_names.has(created_name):
-			return created_name
+			return created_name.capitalize()
 	return fallback_name
 
 func _make_random_name() -> String:
@@ -203,26 +226,124 @@ func _make_random_name() -> String:
 	end_char_full.append_array(end_char)
 	end_char_full.append_array(end_char_alts)
 	var start: String = ""
-	if randi() % 2:
+	if randi() % 2 == 1:
 		start = first_char_full[randi() % first_char_full.size()]
 	var middle: String = end_char[randi() % end_char.size()]
 	var longer_end: String = ""
-	if randi() % 2:
+	if randi() % 2 == 1:
 		longer_end = end_char_full[randi() % end_char_full.size()]
 	var end: String = first_char[randi() % first_char.size()] + longer_end
 	var new_name: String = ""
 	if !new_name.ends_with("y"):
 		new_name = first_char_full[randi() % first_char_full.size()] + middle + end
 	else:
-		if randi() % 2:
+		if randi() % 3 == 1:
 			new_name = start + middle + end_char[randi() % end_char.size()]
 	if new_name.ends_with("j") or new_name.ends_with("w"):
 		new_name.erase(new_name.length() - 1, 1)
 	if new_name.begins_with("nk"):
 		new_name.erase(0, 2)
-	return new_name.capitalize()
+	return new_name
 
-func update_appearance() -> void:
+func _reposition_outfit() -> void:
+	var sticker_pos_x: float  = 0
+	var sticker_pos_y: float  = 0
+	var sticker_rot: float = 0
+	var hat_pos_x: float = 0
+	var hat_pos_y: float = 0
+	var hat_rot_x: float = 0
+	var hat_rot_z: float = 0
+	match shells.find(identity.mesh_shell):
+		0:
+			sticker_pos_x -= 0
+			sticker_pos_y += -0.1
+			sticker_rot = 0
+			hat_pos_x -= 0
+			hat_pos_y -= 0.02
+			hat_rot_x = 8
+			hat_rot_z = 0
+		1:
+			sticker_pos_x -= 0.2
+			sticker_pos_y += 0.25
+			sticker_rot = 90
+			hat_pos_x -= 0
+			hat_pos_y += 0.45
+			hat_rot_x = 0
+			hat_rot_z = -10
+		2:
+			sticker_pos_x -= 0.2
+			sticker_pos_y += 0.25
+			sticker_rot = 90
+			hat_pos_x -= 0
+			hat_pos_y += 0.45
+			hat_rot_x = -5
+			hat_rot_z = 0
+		3:
+			sticker_pos_x -= 0.3
+			sticker_pos_y += 0.11
+			sticker_rot = 90
+			hat_pos_x -= 0
+			hat_pos_y += 0.2
+			hat_rot_x = 0
+			hat_rot_z = -15
+		4:
+			sticker_pos_x -= 0.2
+			sticker_pos_y += 0.25
+			sticker_rot = 90
+			hat_pos_x -= 0
+			hat_pos_y += 0.45
+			hat_rot_x = 0
+			hat_rot_z = -10
+		5:
+			sticker_pos_x -= 0.1
+			sticker_pos_y += 0.25
+			sticker_rot = 50
+			hat_pos_x += 0.1
+			hat_pos_y += 0.2
+			hat_rot_x = -15
+			hat_rot_z = -10
+		6:
+			sticker_pos_x -= 0.5
+			sticker_pos_y += 0.25
+			sticker_rot = 90
+			hat_pos_x -= 0
+			hat_pos_y += 0.53
+			hat_rot_x = -4
+			hat_rot_z = -4
+		7:
+			sticker_pos_x -= 0.2
+			sticker_pos_y += 0.25
+			sticker_rot = 90
+			hat_pos_x -= 0
+			hat_pos_y += 0.45
+			hat_rot_x = 0
+			hat_rot_z = 0
+	sticker.translation.x = sticker_pos_x
+	sticker.translation.y = sticker_pos_y
+	sticker.rotation_degrees.x = sticker_rot
+	hat.translation.x = hat_pos_x
+	hat.translation.y = hat_pos_y
+	hat.rotation_degrees.x = hat_rot_x
+	hat.rotation_degrees.z = hat_rot_z
+
+func _update_appearance_arrays() -> void:
+	var meshes: String = "res://assets/model/"
+	var textures: String = "res://assets/texture/"
+	eyes_left.append_array(Utility.get_loaded_files(meshes, "entity/snail_eye_left", ".mesh"))
+	eyes_right.append_array(Utility.get_loaded_files(meshes, "entity/snail_eye_right", ".mesh"))
+	eye_patterns.append_array(Utility.get_loaded_files(textures, "entity/snail_eyes", ".png"))
+	eyelid_patterns.append_array(Utility.get_loaded_files(textures, "entity/snail_eyelids", ".png"))
+	bodies.append_array(Utility.get_loaded_files(meshes, "entity/snail_body", ".mesh"))
+	body_patterns.append_array(Utility.get_loaded_files(textures, "entity/snail_body_accent", ".png"))
+	shells.append_array(Utility.get_loaded_files(meshes, "entity/snail_shell", ".mesh"))
+	shell_patterns.append_array(Utility.get_loaded_files(textures, "entity/snail_shell_accent", ".png"))
+	stickers.append_array(Utility.get_loaded_files(textures, "object/sticker", ".png"))
+	hats.append_array(Utility.get_loaded_files(meshes, "object/hat", ".mesh"))
+	hat_patterns.append_array(Utility.get_loaded_files(textures, "object/hat", ".png"))
+
+func _update_appearance() -> void:
+	armature.scale = Vector3(identity.entity_scale, identity.entity_scale, identity.entity_scale)
+	collision.shape.radius = identity.entity_scale
 	shell.set_mesh(identity.mesh_shell)
 	body.set_mesh(identity.mesh_body)
 	eye_left.set_mesh(identity.mesh_eye_left)
@@ -245,7 +366,13 @@ func update_appearance() -> void:
 	eye_right_mat.set_shader_param("albedo_color", identity.color_eyes)
 	eyelid_left_mat.set_shader_param("texture_albedo", identity.pattern_eyelids)
 	eyelid_right_mat.set_shader_param("texture_albedo", identity.pattern_eyelids)
-	eyelid_left_mat.set_shader_param("albedo_color", identity.color_body_accent)
-	eyelid_right_mat.set_shader_param("albedo_color", identity.color_body_accent)
-	armature.scale = Vector3(identity.entity_scale, identity.entity_scale, identity.entity_scale)
-	collision.shape.radius = identity.entity_scale
+	eyelid_left_mat.set_shader_param("albedo_color", identity.color_body_base.blend(darkener))
+	eyelid_right_mat.set_shader_param("albedo_color", identity.color_body_base.blend(darkener))
+	hat.set_mesh(identity.mesh_hat)
+	hat_mat.set_shader_param("texture_albedo", identity.pattern_hat)
+	sticker_mat.set_shader_param("texture_albedo", identity.pattern_sticker)
+
+func _notification(what):
+	match what:
+		NOTIFICATION_WM_FOCUS_IN:
+			_update_appearance_arrays()
